727f8f910b7753d84cc4ed4fb7d18e3b48614ab2
solomontikabo.musie <Nordmaling10>
2020-10-05 11:05:18 +0200

    Start på cell & grid

---
+++ src/cell.java
@@ -0,0 +1,26 @@
+public class cell {
+    private int x;
+    private int  y;
+
+    public cell() {
+        int x = 400;
+        int  y = 600;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
---
+++ src/gird.java
@@ -0,0 +1,6 @@
+public class gird {
+    public static void main(String[] args) {
+     cell c3= new cell();
+        System.out.println("That is: " + c3.getY());
+    }
+}
